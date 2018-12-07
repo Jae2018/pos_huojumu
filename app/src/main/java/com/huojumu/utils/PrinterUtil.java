2fe07b26cbd12a31f14421ff72cd9bbae48ad7e1
jae <jae2018game@gmail.com>
2018-12-07 18:00:23 +0800

    打印格式与命令

--- app/src/main/java/com/huojumu/utils/PrinterUtil.java
+++ app/src/main/java/com/huojumu/utils/PrinterUtil.java
@@ -15,6 +15,7 @@ import com.szsicod.print.io.USBAPI;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -34,9 +35,197 @@ public class PrinterUtil {
     private static SimpleDateFormat simpleDateFormat;
     private static Gson gson;
     /**
-     * 是否
+     * 打印纸一行最大的字节
      */
-    private boolean cut_bitmap = false;
+    private static final int LINE_BYTE_SIZE_58 = 32;
+    private static final int LINE_BYTE_SIZE_80 = 48;
+    /**
+     * 打印三列时，中间一列的中心线距离打印纸左侧的距离
+     */
+    private static final int LEFT_LENGTH_58 = 16;
+    private static final int LEFT_LENGTH_80 = 24;
+    /**
+     * 打印三列时，中间一列的中心线距离打印纸右侧的距离
+     */
+    private static final int RIGHT_LENGTH_58 = 16;
+    private static final int RIGHT_LENGTH_80 = 24;
+    /**
+     * 打印三列时，第一列汉字最多显示几个文字
+     */
+    private static final int LEFT_TEXT_MAX_LENGTH_58 = 5;
+    private static final int LEFT_TEXT_MAX_LENGTH_80 = 7;
+
+    /**
+     * 复位打印机
+     */
+    public static final byte[] RESET = {0x1b, 0x40};
+
+    /**
+     * 左对齐
+     */
+    public static final byte[] ALIGN_LEFT = {0x1b, 0x61, 0x00};
+
+    /**
+     * 中间对齐
+     */
+    public static final byte[] ALIGN_CENTER = {0x1b, 0x61, 0x01};
+
+    /**
+     * 右对齐
+     */
+    public static final byte[] ALIGN_RIGHT = {0x1b, 0x61, 0x02};
+
+    /**
+     * 选择加粗模式
+     */
+    public static final byte[] BOLD = {0x1b, 0x45, 0x01};
+
+    /**
+     * 取消加粗模式
+     */
+    public static final byte[] BOLD_CANCEL = {0x1b, 0x45, 0x00};
+
+    /**
+     * 宽高加倍
+     */
+    public static final byte[] DOUBLE_HEIGHT_WIDTH = {0x1d, 0x21, 0x11};
+
+    /**
+     * 宽加倍
+     */
+    public static final byte[] DOUBLE_WIDTH = {0x1d, 0x21, 0x10};
+
+    /**
+     * 高加倍
+     */
+    public static final byte[] DOUBLE_HEIGHT = {0x1d, 0x21, 0x01};
+
+    /**
+     * 字体不放大
+     */
+    public static final byte[] NORMAL = {0x1d, 0x21, 0x00};
+
+    /**
+     * 设置默认行间距
+     */
+    public static final byte[] LINE_SPACING_DEFAULT = {0x1b, 0x32};
+
+    private static int getBytesLength(String msg) {
+        return msg.getBytes(Charset.forName("GB2312")).length;
+    }
+
+    /**
+     * 打印两列
+     *
+     * @param leftText  左侧文字
+     * @param rightText 右侧文字
+     */
+    public static String printTwoData58(String leftText, String rightText) {
+        StringBuilder sb = new StringBuilder();
+        int leftTextLength = getBytesLength(leftText);
+        int rightTextLength = getBytesLength(rightText);
+        sb.append(leftText);
+
+        // 计算两侧文字中间的空格
+        int marginBetweenMiddleAndRight = LINE_BYTE_SIZE_58 - leftTextLength - rightTextLength;
+
+        for (int i = 0; i < marginBetweenMiddleAndRight; i++) {
+            sb.append(" ");
+        }
+        sb.append(rightText);
+        return sb.toString();
+    }
+
+    /**
+     * 打印两列
+     *
+     * @param leftText  左侧文字
+     * @param rightText 右侧文字
+     */
+    public static String printTwoData80(String leftText, String rightText) {
+        StringBuilder sb = new StringBuilder();
+        int leftTextLength = getBytesLength(leftText);
+        int rightTextLength = getBytesLength(rightText);
+        sb.append(leftText);
+
+        // 计算两侧文字中间的空格
+        int marginBetweenMiddleAndRight = LINE_BYTE_SIZE_80 - leftTextLength - rightTextLength;
+
+        for (int i = 0; i < marginBetweenMiddleAndRight; i++) {
+            sb.append(" ");
+        }
+        sb.append(rightText);
+        return sb.toString();
+    }
+
+    /**
+     * 打印三列
+     *
+     * @param leftText   左侧文字
+     * @param middleText 中间文字
+     * @param rightText  右侧文字
+     */
+    public static String printThreeData58(String leftText, String middleText, String rightText) {
+        StringBuilder sb = new StringBuilder();
+        // 左边最多显示 LEFT_TEXT_MAX_LENGTH 个汉字 + 两个点
+        if (leftText.length() > LEFT_TEXT_MAX_LENGTH_58) {
+            leftText = leftText.substring(0, LEFT_TEXT_MAX_LENGTH_58) + "..";
+        }
+        int leftTextLength = getBytesLength(leftText);
+        int middleTextLength = getBytesLength(middleText);
+        int rightTextLength = getBytesLength(rightText);
+
+        sb.append(leftText);
+        // 计算左侧文字和中间文字的空格长度
+        int marginBetweenLeftAndMiddle = LEFT_LENGTH_58 - leftTextLength - middleTextLength / 2;
+
+        for (int i = 0; i < marginBetweenLeftAndMiddle; i++) {
+            sb.append(" ");
+        }
+        sb.append(middleText);
+
+        // 计算右侧文字和中间文字的空格长度
+        int marginBetweenMiddleAndRight = RIGHT_LENGTH_58 - middleTextLength / 2 - rightTextLength;
+
+        for (int i = 0; i < marginBetweenMiddleAndRight; i++) {
+            sb.append(" ");
+        }
+
+        // 打印的时候发现，最右边的文字总是偏右一个字符，所以需要删除一个空格
+        sb.delete(sb.length() - 1, sb.length()).append(rightText);
+        return sb.toString();
+    }
+
+    public static String printThreeData80(String leftText, String middleText, String rightText) {
+        StringBuilder sb = new StringBuilder();
+        // 左边最多显示 LEFT_TEXT_MAX_LENGTH 个汉字 + 两个点
+        if (leftText.length() > LEFT_TEXT_MAX_LENGTH_80) {
+            leftText = leftText.substring(0, LEFT_TEXT_MAX_LENGTH_80) + "..";
+        }
+        int leftTextLength = getBytesLength(leftText);
+        int middleTextLength = getBytesLength(middleText);
+        int rightTextLength = getBytesLength(rightText);
+
+        sb.append(leftText);
+        // 计算左侧文字和中间文字的空格长度
+        int marginBetweenLeftAndMiddle = LEFT_LENGTH_80 - leftTextLength - middleTextLength / 2;
+
+        for (int i = 0; i < marginBetweenLeftAndMiddle; i++) {
+            sb.append(" ");
+        }
+        sb.append(middleText);
+
+        // 计算右侧文字和中间文字的空格长度
+        int marginBetweenMiddleAndRight = RIGHT_LENGTH_80 - middleTextLength / 2 - rightTextLength;
+
+        for (int i = 0; i < marginBetweenMiddleAndRight; i++) {
+            sb.append(" ");
+        }
+
+        // 打印的时候发现，最右边的文字总是偏右一个字符，所以需要删除一个空格
+        sb.delete(sb.length() - 1, sb.length()).append(rightText);
+        return sb.toString();
+    }
 
     //十六进制开钱箱
     public static void OpenMoneyBox() {
